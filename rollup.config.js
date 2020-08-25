655d907d6690b44dcd8102bd1ce4a6a15545c008
Rémy B <remy@buildinamsterdam.com>
2020-08-25 21:56:07 +0200

    update node and rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
 
 module.exports = [
